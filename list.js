c0773977c760dc9ab3c7fd982fbe6e076e623c8b
kitbecker <hasselhoff2k@gmail.com>
2016-11-14 15:59:30 -0500

    add, save and edit JS functions

--- list.js
+++ list.js
@@ -1,3 +1,6 @@
+
+
+/* mark items as done once clicked */
 $(".itemDone").click(function(){
 	var test = $(this).parent().attr('id');
 	var removeTarg = $(this).parent();
@@ -10,3 +13,81 @@ $(".itemDone").click(function(){
 }); 
 
 
+/* See Item Description */
+$(".item-title").click(function(){
+	$(this).parent().toggleClass("edit");
+});
+
+
+/* Add Item Description */
+$(".addDesc").click(function(){
+
+	var parentId = $(this).parent().attr('id');	
+
+	$(this).parent().parent().children(".item-description").append("<textarea>enter a description</textarea>");
+	
+	$("#" + parentId + " .saveDesc").show(300);
+	$("#" + parentId + " .addDesc").hide(300);
+	$("#" + parentId + " .deleteDesc").show(300);
+});
+
+
+/* Edit Item Description */
+$("button.editDesc").click(function(){
+
+	var parentId = $(this).parent().parent().attr('id');	
+
+	var descText  = document.body.querySelector( "#" + parentId + " .item-description" ).innerHTML;
+
+	$(this).parent().parent().children(".item-description").remove();
+
+	var newText = "<div class='item-description'><textarea>" + descText + "</textarea></div>";
+
+	$("#" + parentId + " .saveDesc").show(300);
+	$("#" + parentId + " .editDesc").hide(300);
+	$("#" + parentId ).append(newText);
+
+});
+
+
+/* save Item Description */
+$("button.saveDesc").click(function(){
+
+	var parentId = $(this).parent().parent().attr('id');	
+
+	var descText  = document.body.querySelector( "#" + parentId + " .item-description textarea" ).value;
+
+	$(this).parent().parent().children(".item-description").remove();
+
+	var newText = "<div class='item-description'>" + descText + "</div>";
+
+	$("#" + parentId + " .editDesc").show(300);
+	$("#" + parentId + " .saveDesc").hide(300);
+	$("#" + parentId ).append(newText);
+
+});
+
+
+/* Delete Item Description */
+$("button.deleteDesc").click(function(){
+
+	var parentId = $(this).parent().parent().attr('id');	
+
+	$(this).parent().parent().children(".item-description").remove();
+
+	var newText = "<div class='item-description'></div>";
+
+	$("#" + parentId + " .addDesc").show(300);
+	$("#" + parentId + " .deleteDesc").hide(300);
+	$("#" + parentId ).append(newText);
+
+});
+
+
+
+/* Add Item Description */
+$(".addItem").click(function(){
+
+    $(this).parent().parent().append("<div class='item'><input type='text' placeholder='What do you want to do?' /><button class='saveItem'>+</button></div>");
+
+});
